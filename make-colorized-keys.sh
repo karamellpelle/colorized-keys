e21353100d367d9e406b2883b1b177683067eda2
karamellpelle@hotmail.com <karamellpelle@hotmail.com>
2024-04-03 16:04:48 +0200

    restructuring latex document

--- make-colorized-keys.sh
+++ make-colorized-keys.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DATA_DIR_PANDOC_TEMPLATES="$(pwd)/data-dir/colorized-keys/pandoc/templates/"
 DATA_DIR_GRAPHICS="$(pwd)/graphics/" # / at end is necessary!
+DATA_DIR_SETTINGS="$(pwd)/latex/" 
 
 FILTER_EXE=${FILTER_EXE:-"$(stack path --local-install-root 2> /dev/null)/bin/colorized-filter"}
 echo "using filter $FILTER_EXE"
@@ -8,6 +9,7 @@ echo "using filter $FILTER_EXE"
 pandoc --filter $FILTER_EXE \
        --variable data-dir-graphics=$DATA_DIR_GRAPHICS \
        --variable data-dir-pandoc-templates=$DATA_DIR_PANDOC_TEMPLATES \
+       --variable data-dir-settings=$DATA_DIR_SETTINGS \
        --defaults=./pandoc-colorized.yaml \
        --from=markdown \
        --to=latex \
